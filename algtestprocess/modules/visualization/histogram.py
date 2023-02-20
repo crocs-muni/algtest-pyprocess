84d206b2b69e44c4671c012685bce2d9fbc6d8b7
tjaros <tjaros.822@gmail.com>
2023-02-20 08:39:05 +0100

    refactor: generalize the histogram plots for all dataframes

--- algtestprocess/modules/visualization/histogram.py
+++ algtestprocess/modules/visualization/histogram.py
@@ -7,116 +7,62 @@ from matplotlib import gridspec
 import numpy as np
 
 
-def get_msb_from_hexstring(hexstring: str) -> int:
-    x = int(hexstring, 16)
-    return x >> (
-        x.bit_length() - (8 if x.bit_length() % 8 == 0 else x.bit_length() % 8)
-    )
-
-
 Occurences = List[Union[int, float]]
 Title = XLabel = YLabel = LegendLabel = str
 Bins = int
 Density = bool
-RSAValuesPlotData = Tuple[Occurences, Title, XLabel, YLabel, LegendLabel, Bins, Density]
+HistData = Tuple[Occurences, Title, XLabel, YLabel, LegendLabel, Bins, Density]
 
 
-class RSAValues(Plot):
+class HistogramMix(Plot):
     def __init__(self, grid: Optional[Tuple[int, int]] = None):
         """
-        Constructor of the RSAValues class
+        Constructor of the HistogramMix class
         :param grid: Tuple signifying the (rows, cols) of resulting plot
         """
         super().__init__()
-        self.plots_data: Dict[str, List[RSAValuesPlotData]] = {}
+        self.plots_data: Dict[str, List[HistData]] = {}
         if grid:
             self.rows, self.cols = grid
         else:
             self.rows = None
             self.cols = None
 
-    def add_keygen_time_distribution(
+    def add_distribution(
         self,
         df: pd.DataFrame,
-        rsa: int = 1024,
-        bins: int = 1000,
+        col: str,
+        plot_id: str,
         title: str = "Time of the key generation",
-        time_unit: Tuple[str, int] = ("ms", 1000),
-        name: str = "time distribution",
+        xlabel: XLabel = "time",
+        ylabel: YLabel = "number of occurences",
+        legend: LegendLabel = "unknown",
+        bins: int = 1000,
         density: bool = True,
     ):
         """
         :param df: pandas dataframe
-        :param rsa: rsa bits
+        :param col: column in df containing the values
+        :param plot_id: which plot does data belong to
+        :param xlabel: label for x axis
+        :param ylabel: label for y axis
+        :param legend: legend for particular distribution
         :param bins: histogram bins
         :param title: title of the plot
-        :param time_unit: name and the constant to multiply timo in seconds with
         """
         assert not df.empty
-
-        unit, constant = time_unit
-        time: List[Union[int, float]] = list(
-            map(lambda x: round(float(x) * constant), df.t)
-        )
+        assert col in df.columns
 
         self.add_to_plots_data(
-            time,
+            df[col],
             title,
-            f"time in {unit}",
-            "Density (%)",
-            f"RSA {rsa}",
-            name,
+            xlabel,
+            ylabel,
+            legend,
+            plot_id,
             bins,
             density,
         )
-
-    def add_value_mix_distribution(
-        self,
-        df: pd.DataFrame,
-        rsa: int = 1024,
-        getval_f: Callable[
-            [str | int | float], Union[int, float]
-        ] = get_msb_from_hexstring,
-        columns: List[str] = ["n", "d"],
-        title: str = "MSBs of modulus N vs private d",
-        xlabel: str = "MSB value",
-        ylabel: str = "Density",
-        name: str = "MSB 1024 Nd",
-        bins: int = 1000,
-        density: bool = False,
-    ):
-        """
-        :param df: pandas dataframe
-        :param rsa: rsa bits
-        :param getval_f: function which maps the columns from df into values for dataframe
-        :param columns: which will be plotted into histogram plot
-        :param title: title of the plot
-        :param xlabel: laxel for x axis
-        :param ylabel: laxel for y axis
-        :param name: id of the plot
-        :param bins: histogram bins
-        """
-        assert not df.empty
-
-        for col in columns:
-            xx: List[Union[int, float]] = list(
-                map(lambda x: getval_f(x), list(df[col]))
-            )
-            xxx = {}
-            for x in xx:
-                xxx.setdefault(x, 0)
-                xxx[x] += 1
-            print((col, xxx))
-            self.add_to_plots_data(
-                xx,
-                title,
-                xlabel,
-                ylabel,
-                label=col,
-                name=name,
-                bins=bins,
-                density=density,
-            )
 
     def add_to_plots_data(
         self,
@@ -129,11 +75,14 @@ class RSAValues(Plot):
         bins: int,
         density: bool,
     ):
-        assert xx and title and xlabel and ylabel and label
+        if not isinstance(xx, list):
+            xx = list(xx)
+        assert xx
+        assert title and xlabel and ylabel and label
         self.plots_data.setdefault(name, [])
         self.plots_data[name].append((xx, title, xlabel, ylabel, label, bins, density))
 
-    def histogram(self, ax: plt.Axes, data: List[RSAValuesPlotData]):
+    def histogram(self, ax: plt.Axes, data: List[HistData]):
         set_metadata = False
         for (xx, title, xlabel, ylabel, label, bins, density) in data:
             ax.hist(xx, density=density, bins=bins, label=label, alpha=0.5)
@@ -148,26 +97,31 @@ class RSAValues(Plot):
         assert self.fig is not None
         assert self.cols is not None and self.rows is not None
 
-        items: List[List[RSAValuesPlotData]] = [
+        items: List[List[HistData]] = [
             pl_data for _, pl_data in self.plots_data.items()
         ]
         assert items
 
         i = 0
-        for y in range(self.rows):
-            for x in range(self.cols):
+        for y in range(self.cols):
+            for x in range(self.rows):
                 if i < len(items):
-                    self.histogram(axes[y, x], items[i])
+                    try:
+                        self.histogram(axes[x, y], items[i])
+                    except IndexError:
+                        self.histogram(axes[x], items[i])
                     i += 1
                 else:
-                    self.fig.delaxes(axes[y, x])
+                    self.fig.delaxes(axes[x, y])
 
     @overrides
     def plot(self):
+
         if not self.rows or not self.cols:
-            self.rows = len(self.plots_data) // 2 + 1
-            self.cols = 2
+            # closest square
+            self.cols = self.rows = int(np.ceil(np.sqrt(len(self.plots_data))))
 
         fig, axes = plt.subplots(ncols=self.cols, nrows=self.rows, figsize=(19, 19))
         self.fig = fig
         self.subplots(axes)
+        self.fig.tight_layout()
