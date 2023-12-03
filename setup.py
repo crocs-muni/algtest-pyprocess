ec85590797198afc9b6c8e13a25a4fb7534a29c8
Tomas Jaros <tjaros.822@gmail.com>
2023-12-03 14:45:00 +0100

    feat: create basic setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="algtestprocess",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A Python package for algtestprocess",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Tomas Jaros",
+    author_email="tjaros.822@gmail.com",
+    url="https://github.com/crocs-muni/algtest-pyprocess",
+    install_requires=open("requirements.txt").read().splitlines(),
+)
