91e36b3b9810ab79604718eb40940ac7bb20db9c
Tomas Jaros <tjaros.822@gmail.com>
2024-01-30 21:40:33 +0100

    misc: add requirements to install_requires

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="algtestprocess",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     description="A Python package for algtestprocess",
     long_description=open("README.md").read(),
@@ -10,6 +10,17 @@ setup(
     author="Tomas Jaros",
     author_email="tjaros.822@gmail.com",
     url="https://github.com/crocs-muni/algtest-pyprocess",
-    install_requires=open("requirements.txt").read().splitlines(),
+    install_requires=[
+        "dominate~=2.6.0",
+        "click~=8.1.2",
+        "overrides~=6.1.0",
+        "pandas~=1.4.2",
+        "matplotlib~=3.5.1",
+        "PyYAML~=6.0",
+        "numpy~=1.23.4",
+        "checksumdir~=1.2.0",
+        "tabulate~=0.9.0",
+        "seaborn",
+    ],
     scripts=["bin/pyprocess"],
 )
